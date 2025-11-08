37aa616be469e88c421aaba33fc797f2f28b0031
Sofia <nylund.sofiagreta@gmail.com>
2025-11-08 22:01:35 +0200

    Update map_processing.py

--- software/mapping/map_processing.py
+++ software/mapping/map_processing.py
@@ -1,1 +1,13 @@
-print('Processing map...')
+# map_processing.py
+# TODO: Process LIDAR or depth sensor data
+# Author: Team Name
+
+def load_data(file):
+    pass
+
+def visualize_map():
+    pass
+
+if __name__ == "__main__":
+    print("Mapping module placeholder")
+
